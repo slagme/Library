898f01a6ae7301b79fe1717e3e8a2f44e630f1f9
slagme <slawomir.gmerek@gmail.com>
2017-05-05 22:46:50 +0200

    Added form

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!Doctype HTML>
+<html>
+    <head>
+        <title> Library</title>
+    </head>
+</html>
+
+<body>
+    <p>Add Book:</p>
+
+    <form>
+        Autor: <br>
+        <input type="text" name="author">
+        <br>
+        Title:
+        <input type="text" name="title">
+        <br>
+        Description:
+        <textarea name="description"></textarea>
+        <br>
+        <button>Add book</button>
+    </form>
+
+
+    <p> List of books:</p>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
