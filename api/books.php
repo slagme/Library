d867d3f5c1727c06832c284bd80e025b1edf9357
slagme <slawomir.gmerek@gmail.com>
2017-05-07 21:14:58 +0200

    Function add book added

--- api/books.php
+++ api/books.php
@@ -1,8 +1,8 @@
 <?php
 
 //ładujemy niezbędne pliki
-require_once(__DIR__ . 'api/src/db.php');
-require_once(__DIR__ . 'api/src/Book.php');
+require_once(__DIR__ . '/src/db.php');
+require_once(__DIR__ . '/src/Book.php');
 
 
 //sprawdzamy jakim rodzajem (HTTP) wysłano żądanie
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['id'])){
         //pobieramy id książki
         $id = intval($_GET['id']);
-        $books=Book::loadAllFromDb($conn, $id);
+        $books=Book::loadFromDb($conn, $id);
     } else {
         //pobieramy wszystkie książki
         $books=Book::loadAllFromDb($conn);
@@ -23,18 +23,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     //zwracamy tablicę jsona
     echo json_encode($books);
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $newbook= new Book();
-    $newbook->setAuthor($_POST['author']);
-    $newbook->setTitle($_POST['title']);
-    $newbook->setDescription($_POST['description']);
+    $newBook= new Book();
+    $newBook->setAuthor($_POST['author']);
+    $newBook->setTitle($_POST['title']);
+    $newBook->setDescription($_POST['description']);
 
-    $booksArray=$newbook->create($conn);
+    $booksArray=$newBook->create($conn);
 
     echo json_encode($booksArray);
     
 } elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     //pobranie magiczne danych
     parse_str(file_get_contents("php://input"), $put_vars);
+
+    $id=$put_vars['id'];
+    $bookTitle=$put_vars['title'];
+
+    $bookUpdate= Book::loadFromDb($conn, $id);
+    $result=$bookUpdate->update($conn, $bookTitle);
+
+    echo json_encode($result);
+
+
 } elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
     
     parse_str(file_get_contents("php://input"), $del_vars);
